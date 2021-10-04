0a51eaa1a38e260ae02de305771ea0bf12ba9a00
Van Hung Nguyen <34034513+nguyenvhung9420@users.noreply.github.com>
2021-10-04 10:32:59 +0700

    Added more

--- lib/home/views/home_view.dart
+++ lib/home/views/home_view.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_matching_cards/home/views/sub_views/animated.dart';
+import 'package:flutter_matching_cards/home/views/sub_views/matching_card.dart';
 import 'package:flutter_matching_cards/models/match_card.dart';
 import 'package:flutter_matching_cards/models/piece.dart';
 import 'package:flutter_matching_cards/repositories/people_repository.dart';
@@ -19,10 +20,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<Widget> cardList;
   PeopleRepository api = PeopleRepository();
+  List<MatchCard> cards = new List();
 
   void _removeCard(index) {
     setState(() {
       cardList.removeAt(index);
+    });
+  }
+
+  void _addCard(MatchCard newCard) {
+    setState(() {
+      cards.insert(0, newCard);
     });
   }
 
@@ -59,11 +67,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   List<Widget> _getMatchCard() {
-    List<MatchCard> cards = new List();
-    cards.add(MatchCard(255, 0, 0, 0));
-    cards.add(MatchCard(255, 0, 0, 40));
-    cards.add(MatchCard(0, 255, 0, 80));
-    cards.add(MatchCard(0, 0, 255, 120));
+    _addCard(MatchCard(255, 0, 0, 0));
+    _addCard(MatchCard(255, 0, 0, 40));
+    _addCard(MatchCard(0, 255, 0, 80));
+    _addCard(MatchCard(0, 0, 255, 120));
 
     List<Widget> cardList = new List();
 
@@ -79,106 +86,5 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     return cardList;
-  }
-}
-
-class MatchingCard extends StatefulWidget {
-  MatchCard each;
-  Function() onRemoveCard;
-  int index;
-  MatchingCard({Key key, this.each, this.onRemoveCard, this.index})
-      : super(key: key);
-
-  @override
-  _MatchingCardState createState() => _MatchingCardState();
-}
-
-class _MatchingCardState extends State<MatchingCard> {
-  AnimationController _controller;
-  DragUpdateDetails _details;
-  double width;
-
-  @override
-  void initState() {
-    width = (240 - (widget.index * 10)) * 1.0;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    MatchCard each = widget.each;
-    int index = widget.index;
-    return DraggableCard(
-      matchCard: each,
-      index: index,
-      onRemoveCard: () {
-        widget.onRemoveCard();
-      },
-      child: Material(
-        elevation: 7.0,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: FlutterLogo(size: width),
-        ),
-      ),
-    );
-
-    // Positioned(
-    //   top: each.margin,
-    //   child:
-
-    // Draggable(
-    //   onDragUpdate: (DragUpdateDetails details) {
-    //     setState(() => this._details = details);
-    //     // if (this._details.delta.dx > 10.0) {
-    //     if (this._details.delta.dx > 2.0) {
-    //       widget.onRemoveCard();
-    //       setState(() {
-    //         this.width = this.width - (index * 10);
-    //       });
-    //     }
-    //   },
-    //   onDragEnd: (DraggableDetails details) {},
-    //   childWhenDragging: Container(
-    //     child: Text(
-    //       "dx = " +
-    //           (this._details == null ? "" : this._details.delta.toString()),
-    //     ),
-    //   ),
-    //   feedback: AnimatedContainer(
-    //     duration: const Duration(seconds: 1),
-    //     curve: Curves.bounceInOut,
-    //     width: this.width,
-    //     height: 300,
-    //     decoration: BoxDecoration(
-    //       borderRadius: BorderRadius.circular(10),
-    //       color: Color.fromARGB(
-    //         255,
-    //         each.redColor,
-    //         each.greenColor,
-    //         each.blueColor,
-    //       ).withOpacity(0.7),
-    //     ),
-    //     alignment: Alignment.center,
-    //     child: Text(index.toString()),
-    //   ),
-    //   child: AnimatedContainer(
-    //     duration: const Duration(seconds: 1),
-    //     curve: Curves.bounceInOut,
-    //     width: this.width,
-    //     height: 300,
-    //     decoration: BoxDecoration(
-    //       borderRadius: BorderRadius.circular(10),
-    //       color: Color.fromARGB(
-    //               255, each.redColor, each.greenColor, each.blueColor)
-    //           .withOpacity(0.7),
-    //     ),
-    //     alignment: Alignment.center,
-    //     child: Text(index.toString()),
-    //   ),
-    // ),
-    // );
   }
 }
